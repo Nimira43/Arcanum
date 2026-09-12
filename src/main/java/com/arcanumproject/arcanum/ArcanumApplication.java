4ed88256efaa37f13a9bf0f092a6dc13833d8d85
Nimira43 <n.rabone72@aol.com>
2026-09-12 02:45:47 +0100

    First commit

---
+++ src/main/java/com/arcanumproject/arcanum/ArcanumApplication.java
@@ -0,0 +1,13 @@
+package com.arcanumproject.arcanum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcanumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcanumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arcanumproject/arcanum/ArcanumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcanumproject.arcanum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcanumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
