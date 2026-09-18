6c14ae4f813968ad60d7c46e2e2bbe0ab0426705
Nimira43 <n.rabone72@aol.com>
2026-09-18 02:51:47 +0100

    Assigning categoryId's. Created deleteCategory.

--- src/main/java/com/arcanumproject/arcanum/controller/CategoryController.java
+++ src/main/java/com/arcanumproject/arcanum/controller/CategoryController.java
@@ -2,10 +2,8 @@ package com.arcanumproject.arcanum.controller;
 
 import com.arcanumproject.arcanum.model.Category;
 import com.arcanumproject.arcanum.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,11 +11,8 @@ import java.util.List;
 @RestController
 public class CategoryController {
 
+    @Autowired
     private CategoryService categoryService;
-
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories() {
@@ -28,5 +23,11 @@ public class CategoryController {
     public String createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return "Category added successfully.";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId) {
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
     }
 }
